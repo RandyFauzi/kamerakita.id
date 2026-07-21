836e615e89a8eca2fff0e0fff7bf34c2a1edfe08
Randy Fauzi <randyfauzi24@gmail.com>
2026-07-21 11:06:43 +0800

    feat: add favicon links to export PDF head section

--- resources/views/video-submissions/export-pdf.blade.php
+++ resources/views/video-submissions/export-pdf.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KameraKita.id - {{ date('d-m-Y') }}</title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <!-- Tailwind CSS for high fidelity print styling -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
