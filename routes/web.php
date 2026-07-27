0fe0e413fbc6b63c0802b89d271ad331016bcbee
Randy Fauzi <randyfauzi24@gmail.com>
2026-07-27 16:48:18 +0800

    feat: add secure clear-cache web route to clear server caches directly from browser

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/onboarding/success', function () {
+    return view('onboarding.success');
+})->name('onboarding.success');
+
+Route::get('/clear-cache', function () {
+    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+    return 'Cache cleared successfully!';
+});
+
 Route::get('/get-started', function () {
     return view('get-started');
 })->name('get-started');
