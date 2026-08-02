9527e86fe7e83e8591eb49586fd2a518132efe1f
RandyFauzi <RandyFauzi@users.noreply.github.com>
2026-08-02 22:22:12 +0800

    style: replace send icon with upward tilted variant

--- resources/views/components/mobile-bottom-nav.blade.php
+++ resources/views/components/mobile-bottom-nav.blade.php
@@ -175,8 +175,9 @@
     <a href="{{ route('video-submissions.submit-report.create') }}" class="center-fab-wrapper">
         <div class="fab-glow"></div>
         <div class="fab-button" id="fab-btn">
-            <svg width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.125A59.769 59.769 0 0121.485 12 59.768 59.768 0 013.27 20.875L5.999 12Zm0 0h7.5" />
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m22 2-7 20-4-9-9-4Z"/>
+                <path d="M22 2 11 13"/>
             </svg>
         </div>
     </a>
