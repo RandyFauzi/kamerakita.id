9816892001246b8f9c8abcf4d036fb3d3d3b1350
RandyFauzi <RandyFauzi@users.noreply.github.com>
2026-08-09 22:35:07 +0800

    chore: Try webhook with catch-all again

--- routes/console.php
+++ routes/console.php
@@ -11,6 +11,6 @@ Artisan::command('inspire', function () {
 Schedule::command('partners:sync-activity-status')->dailyAt('00:10');
 Schedule::command('partners:notify-inactive')->dailyAt('09:00');
 
-// Mailbox Catch-all Polling (Dikembalikan ke setiap menit karena Webhook Hostinger tidak mendukung Catch-All)
-Schedule::command('app:pull-mailbox-emails')->everyMinute();
+// Mailbox Cleanup (Karena kita akan mencoba lagi Webhook Catch-all, ini hanya jalan 1x sehari)
+Schedule::command('app:pull-mailbox-emails')->daily();
 Schedule::command('app:clean-expired-emails')->daily();
